7cd43da058e205a4c4ca720f83bc8d8e90d751fd
Pawel Kubiak <pawel.kubiak@olympus-ossa.com>
2020-04-22 21:14:11 +0200

    TDD example

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,9 +1,18 @@
 import { assert } from "chai";
+import { add } from "..";
 
 describe("Array", () => {
   describe("#indexOf()", () => {
     it("should return -1 when the value is not present", () => {
       assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+
+  describe("#add()", () => {
+    it("add 1 and 2 should return 3", () => {
+      const expectedValue: number = 3;
+      const result = add(1, 2);
+      assert.equal(result, expectedValue, 'Result of add is incorect')
     });
   });
 });
